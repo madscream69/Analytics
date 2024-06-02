f46556a6ebb82fd607a187c04bbd070888b168fb
Dolbezhnik <krendelalenkin@gmail.com>
2024-06-02 15:05:07 +0300

    mobile for 375px is done

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,26 @@ const header_btn = document.querySelector('.header__top-menu');
 header_btn.addEventListener('click', ()=>{
     document.querySelector('.menu').classList.toggle('menu--open')
 });
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: "auto",
+      spaceBetween: 20,
+      centeredSlides: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
